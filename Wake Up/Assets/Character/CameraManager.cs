9eb00da512de5b6a92811927f3982a6ed9d48882
SkyBlRb <90752056+SkyBlRb@users.noreply.github.com>
2022-06-16 23:20:37 +0900

    character setting

---
+++ Wake Up/Assets/Character/CameraManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public GameObject target;
+    public float moveSpeed;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(target.gameObject != null)
+        {
+            targetPosition.Set(target.transform.position.x + 5, target.transform.position.y + 3, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Wake Up/Assets/Character/Player.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    public float playerSpeed = 5;
+
+    Rigidbody2D rigid;
+
+    public float jumpPower;
+    public bool isJump = false;
+
+    float horizontal;
+    public Animator anim;
+    public SpriteRenderer rend;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        PlayerMove();
+        Jump();
+    }
+    
+    void PlayerMove()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal != 0)
+        {
+            anim.SetBool("walk", true);
+            if(horizontal > 0)
+            {
+                rend.flipX = false;
+            }
+            else
+            {
+                rend.flipX = true;
+            }
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+
+        Vector3 dir = horizontal * Vector3.right;
+        this.transform.Translate(dir * playerSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (!isJump)
+            {
+                isJump = true;
+                rigid.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag.Equals("ground"))
+        {
+            isJump = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("clearwarp"))
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+        if (other.gameObject.name.Equals("Death"))
+        {
+            SceneManager.LoadScene("GameOverScene");
+        }
+    }
+}
